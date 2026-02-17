4f03c577f41d63898e7f49ec1674f715c98957a3
ajeet kumar <ajeetkumar@Mac.lan>
2026-02-17 23:30:14 +0530

    add serverless

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -4,8 +4,8 @@ let isConnected;
 
 const connectDB = async () => {
   if (isConnected) return;
-
-  const db = await mongoose.connect(process.env.MONGO_URI);
+  const MONGO_URI = `mongodb://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@localhost:27017/${process.env.MONGO_DB_NAME}?authSource=admin`;
+  const db = await mongoose.connect(MONGO_URI);
   isConnected = db.connections[0].readyState;
 };
 
